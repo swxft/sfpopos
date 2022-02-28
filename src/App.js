630ac83453aa8e90b9d8cf6ec072b030ea80a375
swxft <edenweb@outlook.com>
2022-02-28 18:59:15 -0500

    hash router comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Title from './Title';
 import POPOSList from './POPOSList';
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+
 
 function App() {
   return (
